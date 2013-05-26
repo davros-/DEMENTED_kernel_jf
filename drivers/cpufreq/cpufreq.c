d2884aa0d9274e9d4c5526ebc70ab2a3b42a97cc
ktoonsez <ktoonsez@gmail.com>
2013-05-26 13:37:21 -0700

    cpufreq: Send new scaling_min and scaling_max to all cores
    Conflicts:
    	drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,6 +33,8 @@
 #include <trace/events/power.h>
 
 //KT Specifics
+#define CPUS_AVAILABLE	num_possible_cpus()
+
 int GLOBALKT_MIN_FREQ_LIMIT = 378000;
 int GLOBALKT_MAX_FREQ_LIMIT = 1890000;
 
@@ -476,6 +478,7 @@ static ssize_t __ref store_scaling_min_freq(struct cpufreq_policy *policy, const
 {
 	unsigned int ret = -EINVAL;
 	unsigned int value = 0;
+	int cpu;
 	struct cpufreq_policy new_policy;
 
 	ret = sscanf(buf, "%u", &value);
@@ -495,6 +498,20 @@ static ssize_t __ref store_scaling_min_freq(struct cpufreq_policy *policy, const
 	ret = __cpufreq_set_policy(policy, &new_policy);
 	policy->user_policy.min = policy->min;
 
+	//Set extra CPU cores to same speed
+	for (cpu = 1; cpu < CPUS_AVAILABLE; cpu++)
+	{
+		if (!cpu_online(cpu)) cpu_up(cpu);
+		if (&trmlpolicy[cpu] != NULL)
+		{
+			ret = cpufreq_get_policy(&new_policy, cpu);
+			new_policy.min = value;
+			__cpufreq_set_policy(&trmlpolicy[cpu], &new_policy);
+		}
+	}
+	
+	Lbluetooth_scaling_mhz_orig = value;
+	
 	return count;
 }
 
@@ -510,6 +527,7 @@ static ssize_t __ref store_scaling_max_freq(struct cpufreq_policy *policy, const
 
 	if (vfreq_lock == 0)
 	{
+		int cpu;
 		if (value > GLOBALKT_MAX_FREQ_LIMIT)
 			value = GLOBALKT_MAX_FREQ_LIMIT;
 		if (value < GLOBALKT_MIN_FREQ_LIMIT)
@@ -521,6 +539,18 @@ static ssize_t __ref store_scaling_max_freq(struct cpufreq_policy *policy, const
 		new_policy.max = value;
 		ret = __cpufreq_set_policy(policy, &new_policy);
 		policy->user_policy.max = policy->max;
+		
+		//Set extra CPU cores to same speed
+		for (cpu = 1; cpu < CPUS_AVAILABLE; cpu++)
+		{
+			if (!cpu_online(cpu)) cpu_up(cpu);
+			if (&trmlpolicy[cpu] != NULL)
+			{
+				ret = cpufreq_get_policy(&new_policy, cpu);
+				new_policy.max = value;
+				__cpufreq_set_policy(&trmlpolicy[cpu], &new_policy);
+			}				
+		}
 		
 		Lscreen_off_scaling_mhz_orig = value;
 	}
